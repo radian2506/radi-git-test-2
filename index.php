cd669e8c433e69ee60823dbe13f3c072e8c5a23d
radian2506 <radiansyah.pro@gmail.com>
2024-12-26 13:27:55 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur ex velit reiciendis quia, cumque corrupti sint minus recusandae cupiditate? Veniam.</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+    <li>tiga</li>
+  </ul>
 </body>
 </html>
